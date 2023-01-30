7b15c3016031211d6d3cef6dbbdefd94bc1e71b0
SonK4m3 <nguyenhongsont1lhp@gmail.com>
2023-01-30 10:36:52 +0700

    Update: check match face -> change queue to list

--- Assets/Samples/FaceMesh/FacePlayerMovement.cs
+++ Assets/Samples/FaceMesh/FacePlayerMovement.cs
@@ -7,25 +7,25 @@ public class FacePlayerMovement : MonoBehaviour
     [SerializeField] private FaceGameController gameController;
     public RectTransform[] score;
     // make queue to check note following order
-    Queue<FaceNoteMovement> q = new Queue<FaceNoteMovement>();
+    List<FaceNoteMovement> q = new List<FaceNoteMovement>();
     
     public void AddNoteMovement(FaceNoteMovement noteMovement)
     {
         // Debug.Log(noteMovement.gameObject.name + " enqueue");
-        q.Enqueue(noteMovement);
+        q.Add(noteMovement);
     }
 
     public FaceNoteMovement GetNoteMovement()
     {
-        return (q.Count > 0) ? q.Peek() : null;
+        return (q.Count() > 0) ? q.First() : null;
     }
 
     public void NextNote()
     {
-        if (q.Count > 0)
+        if (q.Count() > 0)
         {
             // Debug.Log(q.Dequeue().gameObject.name + " dequeue");
-            q.Dequeue();
+            q.RemoveAt(0);
         }
     }
 
@@ -34,16 +34,14 @@ public class FacePlayerMovement : MonoBehaviour
         return q.Count();
     }
     public FaceNoteMovement CheckFaceNoteMatch(int noteType){
-        FaceNoteMovement target = null;
-        bool isFind = false;
+        FaceNoteMovement res = null;
         for(int i = 0; i < q.Count(); i++){
-            if(q.Peek().NoteType == noteType && !isFind){
-                target = q.Dequeue();
-                isFind = true;
-            } else {
-                q.Enqueue(q.Dequeue());
+            if(q[i].NoteType == noteType){
+                res = q[i];
+                q.RemoveAt(i);
+                break;
             }
         }
-        return target;
+        return res;
     }
 }
